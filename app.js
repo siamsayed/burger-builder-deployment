9c84daceba031d0b28157900d5587fa6b86677c4
Siam sayed <91116558+siamsayed@users.noreply.github.com>
2023-01-10 22:05:59 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const userRouter = require('./routers/userRouter');
 const orderRouter = require('./routers/orderRouter');
 const compression = require('compression');
 
+app.use(cors());
 const app = express();
 app.use(compression())
-app.use(cors());
 app.use(express.json());
 
 app.use('/user', userRouter);
